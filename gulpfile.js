83a028eba24c5152d81c6bcb06268258c8c315c6
Roger Castells <roger.castells@BARLTM3367.local>
2017-08-03 17:46:35 +0200

    Nueva web

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,105 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var plumber = require('gulp-plumber');
-var beep = require('beepbeep');
-var colors = require('colors');
 var browserSync = require('browser-sync').create();
-var imagemin = require('gulp-imagemin');
-var pngquant = require('imagemin-pngquant');
-var ghPages = require('gulp-gh-pages');
-var include = require('gulp-include');
- 
-gulp.task('deploy', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(ghPages());
-});
+var header = require('gulp-header');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var pkg = require('./package.json');
 
-var onError = function(err) {
-  beep([200, 200]);
-  console.log(
-    '\n\n****************************************************\n'.bold.gray +
-    '*****************'.bold.gray + ' \(╯°□°)╯'.bold.red + ' ︵ '.bold.gray +'ɹoɹɹǝ '.bold.blue + '*****************'.bold.gray +
-    '\n****************************************************\n\n'.bold.gray +
-    String(err) +
-    '\n\n*******************************************************\n\n'.bold.gray );
-  this.emit('end');
-};
+// Set the banner content
+var banner = ['/*!\n',
+    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+    ' */\n',
+    ''
+].join('');
 
-
-var input = 'src/SCSS/*.scss';
-var output = 'dist/css';
-
-gulp.task("scripts", function() {
-  console.log("-- gulp is running task 'scripts'");
- 
-  gulp.src("src/js/scrolling-nav.js")
-    .pipe(include())
-      .on('error', console.log)
-    .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('sass', function () {
-  return gulp
-    // Find all `.scss` files from the `stylesheets/` folder
-    .src(input)
-    // Run Sass on those files
-    .pipe(sass())
-    // Write the resulting CSS in the output folder
-    .pipe(gulp.dest(output));
-});
-
-gulp.task('css', function() {
-return gulp.src('src/SCSS/*.scss')
-	.pipe(plumber({
-		errorHandler: onError
-	}))
-	.pipe(sass())
-	.pipe(autoprefixer({
-		browsers:[
-		'last 2 versions',
-		'>1%'
-		]
-	}))
-	.pipe(gulp.dest('dist/css'))
-});
-
-gulp.task('html', function(){
-return gulp.src('src/*.html')
-	.pipe(gulp.dest('dist'))
-});
-
-gulp.task('img', function() {
-    return gulp.src('src/img/*')
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
+// Compile LESS files from /less into /css
+gulp.task('less', function() {
+    return gulp.src('less/agency.less')
+        .pipe(less())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
         }))
-        .pipe(gulp.dest('dist/img'));
 });
 
+// Minify compiled CSS
+gulp.task('minify-css', ['less'], function() {
+    return gulp.src('css/agency.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
 
-gulp.task('default', ['css','html'], function() {
+// Minify JS
+gulp.task('minify-js', function() {
+    return gulp.src('js/agency.js')
+        .pipe(uglify())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// Copy vendor libraries from /node_modules into /vendor
+gulp.task('copy', function() {
+    gulp.src(['node_modules/bootstrap/dist/**/*', '!**/npm.js', '!**/bootstrap-theme.*', '!**/*.map'])
+        .pipe(gulp.dest('vendor/bootstrap'))
+
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('vendor/jquery'))
+
+    gulp.src([
+            'node_modules/font-awesome/**',
+            '!node_modules/font-awesome/**/*.map',
+            '!node_modules/font-awesome/.npmignore',
+            '!node_modules/font-awesome/*.txt',
+            '!node_modules/font-awesome/*.md',
+            '!node_modules/font-awesome/*.json'
+        ])
+        .pipe(gulp.dest('vendor/font-awesome'))
+})
+
+// Run everything
+gulp.task('default', ['less', 'minify-css', 'minify-js', 'copy']);
+
+// Configure the browserSync task
+gulp.task('browserSync', function() {
     browserSync.init({
-        server: {baseDir: "./dist"}, 
-        files:['./dist/css/main.css','./dist/*.html']
-    });
-    gulp.watch('src/scss/**/*.scss',['css']);
-    gulp.watch('src/*.html',['html']);
-    gulp.watch('src/js/*.js',['scripts']);
-    gulp.watch('./dist/*.html').on('change', browserSync.reload)
+        server: {
+            baseDir: ''
+        },
+    })
+})
+
+// Dev task with browserSync
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
+    gulp.watch('css/*.css', ['minify-css']);
+    gulp.watch('js/*.js', ['minify-js']);
+    // Reloads the browser whenever HTML or JS files change
+    gulp.watch('*.html', browserSync.reload);
+    gulp.watch('js/**/*.js', browserSync.reload);
 });
 
-
-
+// Compiles SCSS files from /scss into /css
+// NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
+gulp.task('sass', function() {
+    return gulp.src('scss/agency.scss')
+        .pipe(sass())
+        .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
