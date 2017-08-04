a4163aa818712b5dad4baa9a5833f7f1481feff2
Roger Castells <roger.castells@BARLTM3367.local>
2017-08-04 12:49:10 +0200

    Changing contact and navbar

--- js/googlemaps.js
+++ js/googlemaps.js
@@ -1,8 +1,19 @@
 function myMap() {
+
+var myLatlng = { lat: 41.376692, lng: 2.115948 };
+
 var mapOptions = {
-    center: new google.maps.LatLng(51.5, -0.12),
-    zoom: 10,
-    mapTypeId: google.maps.MapTypeId.HYBRID
+    center: myLatlng,
+    zoom: 16
 }
+
 var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
+var marker = new google.maps.Marker({
+    position: myLatlng,
+    map: map,
+    title: 'Oclusdent',
+    text: 'Oclusdent'
+});
+
 }
